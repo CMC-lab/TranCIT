2c9627104bab84c8f675ab9c58451df826236c59
Salar Nouri <salar.nouri@gmail.com>
2025-03-25 01:49:14 +0330

    Refactor cs nonzero mean for driven harmonic oscillator

--- dcs/causality/get_causal_strength.py
+++ dcs/causality/get_causal_strength.py
@@ -5,100 +5,65 @@ from dcs.utils.preprocessing.singular import regularize_if_singular
 def cs_nonzero_mean(X, morder, time_mode, diag_flag):
     """
     Causal Strength calculation with non-zero mean for bivariate signals.
-    
+
     Parameters:
-    X : np.ndarray
-        The input data array with shape (nvar, nobs, ntrials).
-    morder : int
-        The model order.
-    time_mode : str
-        The mode for time: 'inhomo' for time-inhomogeneous or 'homo' for homogeneous.
-    diag_flag : int
-        A flag indicating whether to use diagonal covariance matrices.
-    
+        X : np.ndarray
+            Input data array of shape (nvar, nobs, ntrials), where nvar is the number of variables,
+            nobs is the number of observations, and ntrials is the number of trials.
+        morder : int
+            Model order (number of lags).
+        time_mode : str
+            Time mode: 'inhomo' for time-inhomogeneous or 'homo' for homogeneous.
+        diag_flag : int
+            Flag to use diagonal covariance matrices (0 = full covariance, 1 = diagonal).
+
     Returns:
-    causal_strength : np.ndarray
-        Causal strength measures.
-    transfer_entropy : np.ndarray
-        Transfer entropy measures.
-    granger_causality : np.ndarray
-        Granger causality measures.
-    coeff_t : np.ndarray
-        The estimated coefficients.
-    transfer_entropy_residual_cov : np.ndarray
-        The residual covariance for transfer entropy.
+        causal_strength : np.ndarray
+            Causal strength measures. Shape: (nobs, 2) for 'inhomo', (1, 2) for 'homo'.
+            Represents the strength of directional influence between variables.
+        transfer_entropy : np.ndarray
+            Transfer entropy measures. Shape: (nobs, 2) for 'inhomo', (1, 2) for 'homo'.
+            Quantifies information transfer between variables.
+        granger_causality : np.ndarray
+            Granger causality measures. Shape: (nobs, 2) for 'inhomo', (1, 2) for 'homo'.
+            Indicates predictive causality based on past values.
+        coeff_t : np.ndarray
+            Estimated coefficients. Shape: (nobs, nvar, nvar * morder + 1).
+        transfer_entropy_residual_cov : np.ndarray
+            Residual covariance for transfer entropy. Shape: (T, 2) where T = nobs - morder.
     """
     nvar, nobs, ntrials = X.shape
     
     r = morder
     r1 = morder + 1
-    XX = np.zeros((nvar, r1, nobs + r1 - 1, ntrials))
+    extended_X = np.zeros((nvar, r1, nobs + r1 - 1, ntrials))
     
     for k in range(r1):
-        XX[:, k, k:k + nobs, :] = X
+        extended_X[:, k, k:k + nobs, :] = X
     
-    X0 = XX[:, 0, r1 - 1:nobs, :]
-    XL = XX[:, 1:r+1, r1 - 1:nobs - 1, :]
+    current_X = extended_X[:, 0, r1 - 1:nobs, :]
+    lagged_X = extended_X[:, 1:r+1, r1 - 1:nobs - 1, :]
     # T = nobs - morder
-    T = X0.shape[1] - 1
+    T = current_X.shape[1] - 1
+    cov_Xp, cov_Yp, C_XYp, C_YXp = compute_covariances(lagged_X, T, morder)
     
     transfer_entropy_residual_cov = np.zeros((T, 2))
     transfer_entropy = np.zeros((T, 2))
     causal_strength = np.zeros((T, 2))
     granger_causality = np.zeros((T, 2))
-    
-    cov_Xp = np.zeros((T, morder, morder))
-    cov_Yp = np.zeros((T, morder, morder))
-    C_XYp = np.zeros((T, morder, morder))
-    C_YXp = np.zeros((T, morder, morder))
         
     coeff_t = np.full((nobs, nvar, nvar * morder + 1), np.nan)
     
     for t in range(T):
-        Ct_0 = np.dot(X0[:, t, :], X0[:, t, :].T) / ntrials
-        YX_lag = np.vstack([XL[:, :morder, t, :].reshape(nvar * morder, ntrials), np.ones((1, ntrials))])
-        Ct_j = np.dot(X0[:, t, :], YX_lag.T) / ntrials
-        Ct_1r = np.dot(YX_lag, YX_lag.T) / ntrials
-        Ct_1r = regularize_if_singular(Ct_1r)
-        Coeff = np.linalg.solve(Ct_1r, Ct_j.T).T
-        SIG = Ct_0 - np.dot(Coeff, np.dot(Ct_1r, Coeff.T))
-        
-        coeff_t[t, :, :] = Coeff
-        
-        A_square = Coeff[:, :-1].reshape(nvar, nvar, morder)
-        
-        a, b = A_square[0, 0, :], A_square[0, 1, :]
-        sigy = SIG[0, 0]
-        
-        c, d = A_square[1, 0, :], A_square[1, 1, :]
-        sigx = SIG[1, 1]
-
-        X_p = XL[1, :, t, :].T
-        Y_p = XL[0, :, t, :].T
-        
-        X_t = X0[1, t, :]
-        Y_t = X0[0, t, :]
-        
-        X_lag = np.hstack([X_p, np.ones((ntrials, 1))])
-        Ct_0_x = np.dot(X_t.T, X_t) / ntrials
-        Ct_j_x = np.dot(X_t.T, X_lag) / ntrials
-        Ct_1r_x = np.dot(X_lag.T, X_lag) / ntrials
-        Ct_1r_x = regularize_if_singular(Ct_1r_x)
-        Coeff_x = np.linalg.solve(Ct_1r_x, Ct_j_x.T).T
-        sigx_r = Ct_0_x - np.dot(Coeff_x, np.dot(Ct_1r_x, Coeff_x.T))
-        
-        Y_lag = np.hstack([Y_p, np.ones((ntrials, 1))])
-        Ct_0_y = np.dot(Y_t.T, Y_t) / ntrials
-        Ct_j_y = np.dot(Y_t.T, Y_lag) / ntrials
-        Ct_1r_y = np.dot(Y_lag.T, Y_lag) / ntrials
-        Ct_1r_y = regularize_if_singular(Ct_1r_y)
-        Coeff_y = np.linalg.solve(Ct_1r_y, Ct_j_y.T).T
-        sigy_r = Ct_0_y - np.dot(Coeff_y, np.dot(Ct_1r_y, Coeff_y.T))
-        
-        cov_Xp[t, :, :] = np.cov(X_p.T)
-        cov_Yp[t, :, :] = np.cov(Y_p.T)
-        C_XYp[t, :, :] = np.cov(X_p.T, Y_p.T)[:nvar, nvar:]
-        C_YXp[t, :, :] = np.cov(Y_p.T, X_p.T)[:nvar, nvar:]
+        coefficients, residual_cov = estimate_coefficients(current_X[:, t, :].T,
+                                                        lagged_X[:, :morder, t, :].reshape(nvar * morder, ntrials).T,
+                                                        ntrials)
+        coeff_t[t, :, :] = coefficients
+        A_square = coefficients[:, :-1].reshape(nvar, nvar, morder)
+        b = A_square[0, 1, :]
+        c = A_square[1, 0, :]
+        sigy = residual_cov[0, 0]
+        sigx = residual_cov[1, 1]
         
         cov_Yp_reg = regularize_if_singular(cov_Yp[t])
         cov_Xp_reg = regularize_if_singular(cov_Xp[t])
@@ -123,20 +88,17 @@ def cs_nonzero_mean(X, morder, time_mode, diag_flag):
                 causal_strength[t, 1] = 0.5 * np.log((sigy + b.T @ np.diag(np.diag(cov_Xp_reg)) @ b) / sigy)
                 causal_strength[t, 0] = 0.5 * np.log((sigx + c.T @ np.diag(np.diag(cov_Yp_reg)) @ c) / sigx)
                 
+            
+            lagged_X_p = lagged_X[1, :, t, :].T
+            lagged_Y_p = lagged_X[0, :, t, :].T
+            current_X_t = current_X[1, t, :]
+            current_Y_t = current_X[0, t, :]
+            _, sigx_r = estimate_coefficients(current_X_t, lagged_X_p, ntrials)
+            _, sigy_r = estimate_coefficients(current_Y_t, lagged_Y_p, ntrials)
             granger_causality[t, 1] = np.log(sigy_r / sigy)
             granger_causality[t, 0] = np.log(sigx_r / sigx)
         
     if time_mode == 'homo':
-        A_square = A_square.reshape(nvar, nvar, morder)
-        
-        a = A_square[0, 0, :]
-        b = A_square[0, 1, :]
-        sigy = SIG[0, 0]
-        
-        c = A_square[1, 0, :]
-        d = A_square[1, 1, :]
-        sigx = SIG[1, 1]
-        
         cov_Xp = np.mean(cov_Xp, axis=0)
         cov_Yp = np.mean(cov_Yp, axis=0)
         Sig_Xp = np.mean(Sig_Xp, axis=0)
@@ -174,3 +136,57 @@ def cs_nonzero_mean(X, morder, time_mode, diag_flag):
         causal_strength = np.vstack([nan_block, causal_strength])
 
     return causal_strength, transfer_entropy, granger_causality, coeff_t, transfer_entropy_residual_cov
+
+def compute_covariances(lagged_data, T, morder):
+    """
+    Compute covariance matrices for lagged data.
+    
+    Parameters:
+    - lagged_data: Lagged data array (nvar x morder x T x ntrials)
+    - T: Number of time steps
+    - morder: Model order (number of lags)
+    
+    Returns:
+    - cov_Xp: Covariance of X past
+    - cov_Yp: Covariance of Y past
+    - C_XYp: Cross-covariance X past to Y past
+    - C_YXp: Cross-covariance Y past to X past
+    """
+    cov_Xp = np.zeros((T, morder, morder))
+    cov_Yp = np.zeros((T, morder, morder))
+    C_XYp = np.zeros((T, morder, morder))
+    C_YXp = np.zeros((T, morder, morder))
+    
+    for t in range(T):
+        X_p = lagged_data[1, :, t, :].T
+        Y_p = lagged_data[0, :, t, :].T
+        cov_Xp[t] = np.cov(X_p.T)
+        cov_Yp[t] = np.cov(Y_p.T)
+        C_XYp[t] = np.cov(X_p.T, Y_p.T)[:morder, morder:]
+        C_YXp[t] = np.cov(Y_p.T, X_p.T)[:morder, morder:]
+    
+    return cov_Xp, cov_Yp, C_XYp, C_YXp
+
+
+def estimate_coefficients(current_data, lagged_data, ntrials):
+    """
+    Compute regression coefficients and residual covariance.
+    
+    Parameters:
+    - current_data: Current time step data (nvar x ntrials)
+    - lagged_data: Lagged data (nvar * morder x ntrials)
+    - ntrials: Number of trials
+    
+    Returns:
+    - Coeff: Regression coefficients
+    - SIG: Residual covariance matrix
+    """
+    lagged_data_ones = np.hstack([lagged_data, np.ones((ntrials, 1))])
+    cross_cov_current = np.dot(current_data.T, current_data) / ntrials
+    cross_cov_between = np.dot(current_data.T, lagged_data_ones) / ntrials
+    auto_cov_lagged = np.dot(lagged_data_ones.T, lagged_data_ones) / ntrials
+    auto_cov_lagged = regularize_if_singular(auto_cov_lagged)
+    coeff = np.linalg.solve(auto_cov_lagged, cross_cov_between.T).T
+    residual_cov = cross_cov_current - np.dot(coeff, np.dot(auto_cov_lagged, coeff.T))
+    
+    return coeff, residual_cov
