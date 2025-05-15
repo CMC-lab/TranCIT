2cfb15088433c0f896303cd64dc16e9989dc2591
Salar Nouri <salar.nouri@gmail.com>
2025-05-16 01:15:02 +0300

    Add logging config for the dcs module

--- dcs/__init__.py
+++ dcs/__init__.py
@@ -3,15 +3,15 @@ __version__ = "0.1.0"
 __author__ = "Salar Nouri"
 __email__ = "salr.nouri@gmail.com"
 
+from .causality import (compute_causal_strength_nonzero_mean,
+                        time_varying_causality)
+from .logger_config import setup_logging
+from .models import (compute_BIC_for_model, compute_multi_trial_BIC,
+                     estimate_var_coefficients, select_model_order)
 from .pipeline import snapshot_detect_analysis_pipeline
-from .causality import compute_causal_strength_nonzero_mean, time_varying_causality
-from .models import (
-    compute_multi_trial_BIC,
-    estimate_var_coefficients,
-    select_model_order,
-    compute_BIC_for_model,
-)
-from .simulation import simulate_ar_event_bootstrap, simulate_ar_event, generate_signals
+from .simulation import (generate_signals, simulate_ar_event,
+                         simulate_ar_event_bootstrap)
 
+setup_logging(log_file="dcs_log.txt")
 
 __all__ = ["causality", "config", "models", "pipeline", "simulation", "utils"]
---
+++ dcs/logger_config.py
@@ -0,0 +1,45 @@
+import logging
+import sys
+
+LOG_LEVEL_DEFAULT = logging.INFO
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+LOGGER_NAME = 'dcs'
+
+
+def setup_logging(name=LOGGER_NAME, level=LOG_LEVEL_DEFAULT, log_format=LOG_FORMAT, date_format=DATE_FORMAT, log_file=None):
+    """
+    Configures logging for the 'dcs' package.
+
+    This should be called once at application start or from __init__.py.
+
+    Args:
+        name (str): Logger name (usually use __name__ or a fixed package name).
+        log_file (str): File to write logs to.
+        level (int): Logging level.
+        log_format (str): Log message format.
+        date_format (str): Timestamp format.
+    """
+    logger = logging.getLogger(name)
+
+    if logger.hasHandlers():
+        return logger
+
+    logger.setLevel(level)
+
+    formatter = logging.Formatter(log_format, datefmt=date_format)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(level)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    if log_file:
+        file_handler = logging.FileHandler(log_file, mode='a')
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(level)    
+        logger.addHandler(file_handler)
+
+    logger.info("Logging configured for '%s'", name)
+    return logger
+
